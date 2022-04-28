1ad71adf64a95262df00961c15a783b2f1c1f806
julioaraujo96 <julioaraujo.bs@gmail.com>
2022-04-28 20:12:04 +0100

    routing

--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Breed } from 'src/app/models/breeds';
 import { Fact } from 'src/app/models/fact';
 import { CatinfoService } from 'src/app/services/catinfo.service';
@@ -13,13 +14,18 @@ export class HomepageComponent implements OnInit {
   breeds: Breed[] = [];
   fact: Fact[] = [];
 
-  constructor( private catInfoService: CatinfoService,) { }
+  constructor( private catInfoService: CatinfoService, private route: Router) { }
 
   ngOnInit(): void {
     this.catInfoService.allCatBreeds$.subscribe((data: Breed[]) => {
       console.log(data);
       this.breeds = data;
     });
+  }
+
+  showDetailPage()
+  {
+    this.route.navigate(['details']);
   }
 
 }
